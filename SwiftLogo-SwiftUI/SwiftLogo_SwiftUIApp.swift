5d59ed30b874a84968bab41adb3dff4a3a4cbb42
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-30 21:46:18 +0700

    Initial Commit

---
+++ SwiftLogo-SwiftUI/SwiftLogo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLogo_SwiftUIApp.swift
+//  SwiftLogo-SwiftUI
+//
+//  Created by Алексей Синяговский on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLogo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
